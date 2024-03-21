5536dcaf897e8da50a455fbaa10fc65989eb4288
rachnaraj <rachnaraj1508@gmail.com>
2024-03-21 19:58:34 -0400

    adding diff of src code and dst code in a new column

--- Dataset/test_data_preprocessing.py
+++ Dataset/test_data_preprocessing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+import difflib
 
 # Read the JSON file into a DataFrame
 df_test = pd.read_json(r"D:\Me\concordia\Notes\GenerativeAI\Project\Implementation\dataset\dataset\test.jsonl", lines=True)
@@ -53,6 +54,21 @@ print("Minimum token count of src_javadoc:", min_src_tokens)
 print("Maximum token count of src_javadoc:", max_src_tokens)
 print("Minimum token count of dst_javadoc:", min_dst_tokens)
 print("Maximum token count of dst_javadoc:", max_dst_tokens)
+
+# adding diff column in the dataset... 
+def generate_diff(old_code, new_code):
+    old_code_lines = old_code.splitlines()
+    new_code_lines = new_code.splitlines()
+ 
+    diff = difflib.unified_diff(old_code_lines, new_code_lines)
+ 
+    return '\n'.join(list(diff))
+
+# Apply the generate_diff function to each pair of 'src_method' and 'dst_method' and store the result in a new column 'diff'
+df_filtered['diff'] = df_filtered.apply(lambda row: generate_diff(row['src_method'], row['dst_method']), axis=1)
+
+
+
 
 # dropping unnecessary columns..
 columns_to_drop = ['code_change_seq', 'index', 'src_desc', 'dst_desc', 'src_desc_tokens', 'dst_desc_tokens', 'desc_change_seq', 'dist', 'dst_javadoc_tokens', 'src_javadoc_tokens']
--- Dataset/train_data_preprocessing.py
+++ Dataset/train_data_preprocessing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-
+import difflib
 # Read the JSON file into a DataFrame
 df_train = pd.read_json(r"D:\Me\concordia\Notes\GenerativeAI\Project\Implementation\dataset\dataset\train.jsonl", lines=True)
 
@@ -36,6 +36,21 @@ print("Maximum token count of dst_javadoc:", max_dst_tokens)
 # dropping unnecessary columns..
 columns_to_drop = ['code_change_seq', 'index', 'src_desc', 'dst_desc', 'src_desc_tokens', 'dst_desc_tokens', 'desc_change_seq', 'dist', 'dst_javadoc_tokens', 'src_javadoc_tokens']
 df_filtered.drop(columns=columns_to_drop, inplace=True)
+
+# adding diff column in the dataset...
+
+ 
+def generate_diff(old_code, new_code):
+    old_code_lines = old_code.splitlines()
+    new_code_lines = new_code.splitlines()
+ 
+    diff = difflib.unified_diff(old_code_lines, new_code_lines)
+ 
+    return '\n'.join(list(diff))
+
+# Apply the generate_diff function to each pair of 'src_method' and 'dst_method' and store the result in a new column 'diff'
+df_filtered['diff'] = df_filtered.apply(lambda row: generate_diff(row['src_method'], row['dst_method']), axis=1)
+
 # Storing filtered data to a csv file.
 csv_file_path = r"D:\Me\concordia\Notes\GenerativeAI\Project\Implementation\train_preprocessed.csv"
 
