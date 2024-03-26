c90d14d89629eb064ec297b8da6b25b5e1281d9d
rachnaraj <rachnaraj1508@gmail.com>
2024-03-26 11:16:17 -0400

    gemma script

--- Training-Script-Gemma/Gemma.sh
+++ Training-Script-Gemma/Gemma.sh
@@ -5,7 +5,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=rachna.raj@mail.concordia.ca
 
-source /home/t_ereira/myenvs/bin/activate
-conda activate /home/t_ereira/myenvs/envs/cerberus
+source /home/rraj/myenvs/bin/activate
+conda activate /home/rraj/myenvs/envs/cerberus
 
 python "/home/rraj/GenAI/TrainingScriptExp1.py" "/home/rraj/GenAI/train_preprocessed.csv" "/home/rraj/GenAI/test_preprocessed.csv" "/home/rraj/GenAI/Results/"
